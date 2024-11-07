5a455abe5c605f55294e55726f5006a4f683fd89
Tomek <tomekz@vp.pl>
2024-11-07 11:39:39 +0100

    feat: add the main logo

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 // import useTranslation from "hooks/useTranslation";
+import LindenLogo from "assets/LindenLogo-removebg.png";
 
 function HomePage() {
   // const [translation] = useTranslation();
 
   return (
     <>
+      <img src={LindenLogo} alt="Linden Home Community Logo" />
       <p>Linden Home Community</p>
       {/* <PageTitle title="Home" /> */}
     </>
